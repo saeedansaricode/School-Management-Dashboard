8652fb3374bc60975a39f5099d7a860eab14621a
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-04-30 13:26:29 +0330

    switch to use client component,  write useState and useEffect darkMode hooks in ThemeToggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,24 @@
+"use client"
+
+import { useEffect, useState } from "react";
 
 function ThemeToggle() {
+  const [darkMode, setDarkMode] = useState(true);
 
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") setDarkMode(true);
+  }, []);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]); 
 
   return (
     <div>ThemeToggle</div>
