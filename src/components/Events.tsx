c2cd4065c03660a56867e5a1cfdb0682ff332d0c
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-01 14:33:03 +0330

    add dynamic event

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -13,50 +13,67 @@ interface Event {
   description: string;
 }
 async function Events() {
-  const eventsData = await fetchApi<Event[]>(
-    "http://localhost:4000/eventsData"
-  );
+  const url = "http://localhost:4000/eventsData"
+  const eventsData = await fetchApi<Event[]>(url);
   const date = CurrentDate();
 
   if (!eventsData) {
-    return <p>Error fetching data</p>;
-  }
-
-  return (
-    <div className="bg-white dark:bg-medium rounded-xl p-4">
-      {/* HEADER */}
-      <div className="flex justify-between items-center my-2">
-        <h1 className=" text-lg font-semibold">Events</h1>
-
-        <Link href="/list/events">
-          <span className="text-xs text-gray-300"> View All</span>
-        </Link>
+    return (
+      <div className="bg-white rounded-xl p-4">
+        {/* HEADER */}
+        <div className="flex justify-between items-center my-2">
+          <h1 className="text-lg font-semibold">Events</h1>
+          <Link href="/list/events">
+            <span className="text-xs text-gray-300">View All</span>
+          </Link>
+        </div>
+        {/* BODY */}
+        <div className="flex flex-col gap-4 mt-4">
+          <p className="flex justify-center items-center mt-2 text-gray-400 text-sm">Data Fetch Error</p>
+        </div>
       </div>
-
-      {/* EVENTS */}
-      <div className="flex flex-col gap-4">
-        {eventsData.map((event) => {
-          if (date === event.date)
-            return (
-              <div
-                className="p-2 rounded-md border-2 border-gray-100 bor-t-4 odd:border-t-schoolBlue even:border-t-schoolPurple dark:border-none dark:odd:bg-schoolLightBlue dark:even:bg-schoolLightPurple"
-                key={event.id}
-              >
-                <div className="flex justify-between items-center">
-                  <h1 className="font-semibold text-gray-600">{event.title}</h1>
-                  <span className="text-xs text-gray-300">
-                    {event.startTime} - {event.endTime}
-                  </span>
+    );
+  } else if (eventsData) {
+    return (
+      <div className="bg-white rounded-xl p-4">
+        {/* HEADER */}
+        <div className="flex justify-between items-center my-2">
+          <h1 className="text-lg font-semibold">Events</h1>
+          <Link href="/list/events">
+            <span className="text-xs text-gray-300">View All</span>
+          </Link>
+        </div>
+        {/* body */}
+        <div className="flex flex-col gap-4 mt-4">
+          {eventsData.map((event) => {
+            if (date === event.date) {
+              return (
+                <div
+                  className="bg-schoolLightBlue round-md p-2"
+                  key={event.id}
+                >
+                  <div className="flex items-center justify-between">
+                    <h1 className="font-semibold text-gray-600">
+                      {event.title}
+                    </h1>
+                    <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+                      {event.date}
+                    </span>
+                  </div>
+                  <p className="mt-2 text-gray-400 text-sm">
+                    {event.description}
+                  </p>
                 </div>
-                <p className="mt-2 text-gray-400 text-sm">
-                  {event.description}
-                </p>
-              </div>
-            );
-        })}
+              );
+            }
+          })}
+          <div>
+            <p className="flex justify-center items-center mt-2 text-gray-400 text-sm">No event today</p>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Events;
