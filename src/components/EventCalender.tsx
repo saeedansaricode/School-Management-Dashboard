dec80345cee6bf23001c6c5c56a4dfb452352893
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-11 11:20:04 +0330

    Delete Events from EventCalender

--- src/components/EventCalender.tsx
+++ src/components/EventCalender.tsx
@@ -41,33 +41,6 @@ const EventCalender = () => {
       <div className="flex justify-center items-center dark:text-dark">
         <Calendar onChange={onChange} value={value} />
       </div>
-      {/* HEADER */}
-      <div className="flex justify-between items-center my-2">
-        <h1 className=" text-lg font-semibold">Events</h1>
-        
-        <Link href="/list/events">
-          {/* <Image src="/moreDark.png" alt="more" width={20} height={20} /> */}
-          <span className="text-xs text-gray-300"> View All</span>
-        </Link>
-      </div>
-      {/* EVENTS */}
-      <div className="flex flex-col gap-4">
-        {eventsData.map((event) => {
-          return (
-            <div
-              className="p-2 rounded-md border-2 border-gray-100 bor-t-4 odd:border-t-schoolBlue even:border-t-schoolPurple dark:border-none dark:odd:bg-schoolLightBlue dark:even:bg-schoolLightPurple"
-              key={event.id}
-            >
-              <div className="flex justify-between items-center">
-                <h1 className="font-semibold text-gray-600">{event.title}</h1>
-                <span className="text-xs text-gray-300">{event.time}</span>
-              </div>
-              <p className="mt-2 text-gray-400 text-sm">{event.descriprion}</p>
-            </div>
-          );
-        })}
-      </div>
-
     </div>
   );
 };
