0474a479cb3dfe81929bcf284d2856db18e0107b
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-17 22:28:23 +0330

    write Prev and Next button condition

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -4,19 +4,25 @@ import { useRouter } from "next/navigation";
 
 function Pagination({ page, count }: { page: number; count: number }) {
   // DYNAMIC ROUTE
-  const router = useRouter()
+  const router = useRouter();
   const changePage = (newPage: number) => {
     const params = new URLSearchParams(window.location.search);
     params.set("page", newPage.toString());
     router.push(`${window.location.pathname}?${params}`);
   };
 
+  // PREV AND NEXT BUTTON CONDITION
+  const hasPerv = ITEM_PER_PAGE * (page - 1) > 0;
+  const hasNext = ITEM_PER_PAGE * (page - 1) + ITEM_PER_PAGE < count;
+
   return (
     <div className="flex justify-between items-center text-gray-500">
       <button
         disabled
         className="rounded-sm bg-slate-200 py-2 px-2 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
-        onClick={()=>{changePage(page - 1)}}
+        onClick={() => {
+          changePage(page - 1);
+        }}
       >
         Prev
       </button>
@@ -46,7 +52,9 @@ function Pagination({ page, count }: { page: number; count: number }) {
       <button
         disabled
         className="rounded-sm bg-slate-200 py-2 px-2 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
-        onClick={()=>{changePage(page + 1)}}
+        onClick={() => {
+          changePage(page + 1);
+        }}
       >
         Next
       </button>
