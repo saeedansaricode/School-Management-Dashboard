2a7953ac78fe0e5d5af011a954ad1880ea2da044
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-17 22:21:54 +0330

    fix useRouter's bug

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 function Pagination({ page, count }: { page: number; count: number }) {
-  
   // DYNAMIC ROUTE
-  const router = useRouter();
+  const router = useRouter()
   const changePage = (newPage: number) => {
     const params = new URLSearchParams(window.location.search);
     params.set("page", newPage.toString());
@@ -17,6 +16,7 @@ function Pagination({ page, count }: { page: number; count: number }) {
       <button
         disabled
         className="rounded-sm bg-slate-200 py-2 px-2 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+        onClick={()=>{changePage(page - 1)}}
       >
         Prev
       </button>
@@ -33,6 +33,9 @@ function Pagination({ page, count }: { page: number; count: number }) {
                 className={`px-2 rounded-sm ${
                   page === pageIndex ? "bg-schoolBlue" : ""
                 }`}
+                onClick={() => {
+                  changePage(pageIndex);
+                }}
               >
                 {pageIndex}
               </button>
@@ -43,6 +46,7 @@ function Pagination({ page, count }: { page: number; count: number }) {
       <button
         disabled
         className="rounded-sm bg-slate-200 py-2 px-2 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+        onClick={()=>{changePage(page + 1)}}
       >
         Next
       </button>
