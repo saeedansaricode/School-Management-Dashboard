609abe87b7a951c3d5ef625d3f6cbb6d4944c475
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-18 11:42:36 +0330

    set onSubmit event to form

--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -8,7 +8,7 @@ const TableSearch = () => {
   const handleSubmit = () => {}
 
   return (
-    <form className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+    <form onSubmit={handleSubmit} className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
       <Image src="/search.png" alt="searchbar" width={14} height={14} />
       <input
         type="text"
