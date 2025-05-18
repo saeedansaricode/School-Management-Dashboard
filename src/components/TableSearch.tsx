0c11459d1602c5863d7ebad17940c1924df8f9c8
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-18 11:41:45 +0330

    write handleSubmit function

--- src/components/TableSearch.tsx
+++ src/components/TableSearch.tsx
@@ -1,7 +1,12 @@
+"use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const TableSearch = () => {
+  // DYNAMIC ROUTE
+  const router = useRouter();
+  const handleSubmit = () => {}
+
   return (
     <form className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
       <Image src="/search.png" alt="searchbar" width={14} height={14} />
