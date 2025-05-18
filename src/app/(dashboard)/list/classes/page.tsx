3dfd8208530d25a15c49462192aecaf3f3e87ee6
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-18 14:57:22 +0330

    set type to renderRow

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -35,7 +35,7 @@ const columns = [
   },
 ];
 
-const renderRow = (item: Class) => (
+const renderRow = (item: ClassList) => (
   <tr
     key={item.id}
     className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-schoolLightPurple dark:bg-medium dark:hover:bg-dark"
