8c43e5ced1975c1cf89549e5731c399a9015445c
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-19 15:45:15 +0330

    set dynamic value to CountChartContainer

--- src/components/CountChartContainer.jsx
+++ src/components/CountChartContainer.jsx
@@ -27,16 +27,16 @@ async function CountChartContainer() {
       <div className="flex justify-center gap-16">
         <div className="flex flex-col gap-1">
           <div className="w-4 h-4 bg-schoolBlue rounded-full" />
-          <h1 className="font-bold">{male ? male : 0}</h1>
+          <h1 className="font-bold">{boys}</h1>
           <h2 className="text-xs text-gray-300">
-            {data[2].name + " " + data[2].count}%
+            Boys ({Math.round((boys / (boys + girls)) * 100)}%)
           </h2>
         </div>
         <div className="flex flex-col gap-1">
           <div className="w-4 h-4 bg-schoolYellow rounded-full" />
-          <h1 className="font-bold">{female ? female : 0}</h1>
+          <h1 className="font-bold">{girls}</h1>
           <h2 className="text-xs text-gray-300">
-            {data[1].name + " " + data[1].count}%
+            Girls ({Math.round((girls / (boys + girls)) * 100)}%)
           </h2>
         </div>
       </div>
