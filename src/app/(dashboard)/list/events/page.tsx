5063474989280615cc7effa0425cc8a4a7978099
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-19 13:27:56 +0330

    copy searchParams and dataFetching function

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -72,8 +72,65 @@ const renderRow = (item: Event) => (
     </td>
   </tr>
 );
-async function EventListPage () {
+async function EventListPage ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  // GET SEARCHPARAMS AND CALCULATING PAGENUMBER INFO
+  const { page, ...queryParams } = await searchParams;
+  const p = page ? parseInt(page) : 1;
 
+  // URL PARAMS CONDITION
+  const query: Prisma.ExamWhereInput = {};
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          // FOR SINGLE STUDENT PAGE
+          case "classId":
+            query.lesson = { classId : parseInt(value) }
+            break;
+
+          // FOR SINGLE TEACHER PAGE
+          case "teacherId":
+            query.lesson = { teacherId : value }
+            break;
+
+          // SEARCHING PARAMS
+          case "search":
+            query.lesson = {
+              subject: { name: { contains: value, mode: "insensitive" } },
+            };
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  const [data, count] = await prisma.$transaction([
+    prisma.exam.findMany({
+      where: query,
+      include: {
+        lesson: {
+          select: {
+            subject: { select: { name: true } },
+            class: { select: { name: true } },
+            teacher: { select: { name: true, surname: true } },
+          },
+        },
+      },
+
+      // DATA FETCHING OPTIMIZATION
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+
+    // GET ALL DATA LENGTH
+    prisma.exam.count({ where: query }),
+  ]);
   return (
     <div className="flex flex-col p-4 bg-white rounded-lg m-4 mt-0  dark:bg-medium">
       {/* HEADER */}
