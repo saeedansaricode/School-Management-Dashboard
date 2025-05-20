936d766f611dc8a2aef4f7630cc06666d186685a
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-20 11:22:39 +0330

    copy chart container from AttendanceChart to AttendanceChartContainer

--- src/components/AttendanceChartContainer.jsx
+++ src/components/AttendanceChartContainer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function AttendanceChartContainer() {
   return (
-    <div>AttendanceChartContainer</div>
-  )
+    <div className=" bg-white dark:bg-medium rounded-xl w-full h-full p-4">
+      {/* HEADER */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-lg font-semibold">Attendance</h1>
+        <Link href="/list/attendances">
+          <Image src="/moreDark.png" alt="more" width={20} height={20} />
+        </Link>
+      </div>
+
+      {/* CHART */}
+    </div>
+  );
 }
 
-export default AttendanceChartContainer
+export default AttendanceChartContainer;
