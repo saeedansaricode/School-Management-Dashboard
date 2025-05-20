a723a594514aa45a3460f1b84caf242ac337b755
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-20 12:16:50 +0330

    fetch data

--- src/components/AttendanceChartContainer.jsx
+++ src/components/AttendanceChartContainer.jsx
@@ -1,15 +1,29 @@
 import Image from "next/image";
 import AttendanceChart from "./AttendanceChart";
 import Link from "next/link";
+import { prisma } from "@/lib/prisma";
 
-function AttendanceChartContainer() {
-    // GET CURRENT WEEK'S DAYS FROM LAST MONDAY
-    const today = new Date()
-    const dayOfWeek = today.getDay()
-    const daysSinceMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1
-    const lastMonday = new Date(today)
-    lastMonday.setDate(today.getDate() - daysSinceMonday)
-    
+async function AttendanceChartContainer() {
+  // GET CURRENT WEEK'S DAYS FROM LAST MONDAY
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const daysSinceMonday = dayOfWeek === 0 ? 6 : dayOfWeek - 1;
+  const lastMonday = new Date(today);
+  lastMonday.setDate(today.getDate() - daysSinceMonday);
+
+  // FETCH DATA
+  const dataRes = await prisma.attendance.findMany({
+    where: {
+      date: {
+        gte: lastMonday,
+      },
+    },
+    select: {
+      date: true,
+      present: true,
+    },
+  });
+
   return (
     <div className=" bg-white dark:bg-medium rounded-xl w-full h-full p-4">
       {/* HEADER */}
