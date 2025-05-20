b43b88a0bf2d80b42738aa3516bc450fb09922c5
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-20 13:30:08 +0330

    write data type

--- src/components/AttendanceChart.tsx
+++ src/components/AttendanceChart.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
-import React, { PureComponent } from "react";
+import React from "react";
 import {
   BarChart,
   Bar,
@@ -15,40 +13,9 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Sat",
-    peresent: 60,
-    absent: 40,
-  },
-  {
-    name: "Sun",
-    peresent: 70,
-    absent: 60,
-  },
-  {
-    name: "Mon",
-    peresent: 90,
-    absent: 75,
-  },
-  {
-    name: "Tue",
-    peresent: 80,
-    absent: 45,
-  },
-  {
-    name: "Wed",
-    peresent: 60,
-    absent: 25,
-  },
-  {
-    name: "Thu",
-    peresent: 50,
-    absent: 10,
-  },
-];
+type Data = { name: string; present: number; absent: number }[]
 
-const AttendanceChart = () => {
+function AttendanceChart() {
   return (
     <div className="w-full h-[93%]">
       <ResponsiveContainer>
@@ -70,7 +37,7 @@ const AttendanceChart = () => {
             wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
           />
           <Bar
-            dataKey="peresent"
+            dataKey="present"
             fill="#C3EBFA"
             legendType="circle"
             radius={[10, 10, 0, 0]}
@@ -85,6 +52,6 @@ const AttendanceChart = () => {
       </ResponsiveContainer>
     </div>
   );
-};
+}
 
 export default AttendanceChart;
