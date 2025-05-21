afd9d6c1f270ed94f287e1a2133ca78070d00f9b
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-21 16:20:17 +0330

    write EventCalendarContainer body

--- src/components/EventCalendarContainer.tsx
+++ src/components/EventCalendarContainer.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 async function EventCalendarContainer({
@@ -7,7 +9,29 @@ async function EventCalendarContainer({
 }) {
   // GET SEARCH PARAMS AS DATA
   const { data } = await searchParams;
-  return <div>EventCalendarContainer</div>;
+  return (
+    // CONTAINAR ( CALENDAR + EVENTS )
+    <div className="bg-white dark:bg-medium p-4 rounded-md">
+      {/* CALENDAR */}
+
+      {/* EVENT */}
+      {/* HEADER */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold my-4">Events</h1>
+
+        {/* ALL EVENTS */}
+        <Link href="/list/events">
+          <Image src="/moreDark.png" alt="" width={20} height={20} />
+        </Link>
+      </div>
+
+      {/* BODY */}
+      <div className="flex flex-col gap-4">
+
+        {/* EVENT LIST */}
+      </div>
+    </div>
+  );
 }
 
 export default EventCalendarContainer;
