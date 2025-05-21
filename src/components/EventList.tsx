d930e80bfc96f27068642e27b3871a041d74f565
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-21 16:35:03 +0330

    write function body

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -12,7 +12,24 @@ async function EventList({ dateParam }: { dateParam: string | undefined }) {
       },
     },
   });
-  return <div>EventList</div>;
+  return data.map((event) => (
+    <div
+      className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-schoolBlue even:border-t-schoolPurple"
+      key={event.id}
+    >
+      <div className="flex items-center justify-between">
+        <h1 className="font-semibold text-gray-600">{event.title}</h1>
+        <span className="text-gray-300 text-xs">
+          {event.startTime.toLocaleTimeString("en-UK", {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+          })}
+        </span>
+      </div>
+      <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+    </div>
+  ));
 }
 
 export default EventList;
