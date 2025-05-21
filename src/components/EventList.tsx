054b4a85dfabe2c724598c99cfe489671b062314
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-21 16:29:38 +0330

    fetch data

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { prisma } from "@/lib/prisma";
+import React from "react";
 
-function EventList({dateParam} : {dateParam : string | undefined}) {
-    const date = dateParam ? new Date(dateParam) : new Date()
-  return (
-    <div>EventList</div>
-  )
+async function EventList({ dateParam }: { dateParam: string | undefined }) {
+  const date = dateParam ? new Date(dateParam) : new Date();
+
+  const data = await prisma.event.findMany({
+    where: {
+      startTime: {
+        gte: new Date(date.setHours(0, 0, 0, 0)),
+        lte: new Date(date.setHours(23, 59, 59, 999)),
+      },
+    },
+  });
+  return <div>EventList</div>;
 }
 
-export default EventList
+export default EventList;
