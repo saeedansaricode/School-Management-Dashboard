85086c90882c7e0aa34e0fcd903211b19613526a
Saeed_Ansari <saeed4641ansari@gmail.com>
2025-05-21 16:26:52 +0330

    set new date

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function EventList({dateParam} : {dateParam : string | undefined}) {
+    const date = dateParam ? new Date(dateParam) : new Date()
   return (
     <div>EventList</div>
   )
